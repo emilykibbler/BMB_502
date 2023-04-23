f39a3ac2e1a2fbd56ee09423cdc3a35acc146afc
Emily Kibbler <emilykibbler@gmail.com>
2023-04-23 13:18:25 -0400

    renaming "reference genes" to more correct "housekeeping genes"

--- housekeeping_genes.R
+++ housekeeping_genes.R
@@ -1,5 +1,5 @@
 #Author: Emily Kibbler
-#This code is to evaluate potential reference genes for follow-up studies by qPCR
+#This code is to evaluate potential housekeeping genes for follow-up studies by qPCR
 
 #install.packages("readxl")
 #install.packages("tidyverse")
@@ -13,17 +13,17 @@ all_data<- all_data %>% rename("5057" = "5057-3") #same
 all_data$AveExpr<-rowMeans(all_data[,5:40])
 
 #filter data to include genes with log2fc <1 and average expression >10000 reads
-ref_genes<-subset(all_data,all_data$log2FoldChange<1&all_data$log2FoldChange>-1&all_data$AveExpr>10000)
-nrow(ref_genes) #454 candidate genes at this point
-ref_genes$stdev<-NA
-for(i in 1:nrow(ref_genes)){
-  ref_genes$stdev[i]<-sd(ref_genes[i,5:40])
+HK_genes<-subset(all_data,all_data$log2FoldChange<1&all_data$log2FoldChange>-1&all_data$AveExpr>10000)
+nrow(HK_genes) #454 candidate genes at this point
+HK_genes$stdev<-NA
+for(i in 1:nrow(HK_genes)){
+  HK_genes$stdev[i]<-sd(HK_genes[i,5:40])
 }
-ref_genes$var<-ref_genes$stdev/ref_genes$AveExpr #standard deviation as a fraction of mean reads
+HK_genes$var<-HK_genes$stdev/HK_genes$AveExpr #standard deviation as a fraction of mean reads
 #take the top 20 least variable genes
-ref_genes<-ref_genes[order(ref_genes$var),][1:20,]
+HK_genes<-HK_genes[order(HK_genes$var),][1:20,]
 
-view(ref_genes[order(ref_genes$AveExpr,decreasing=FALSE),])
+view(HK_genes[order(HK_genes$AveExpr,decreasing=FALSE),])
 
 
 
