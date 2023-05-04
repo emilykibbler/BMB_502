cf94b856d81d66ed55f1c1aec9c687ceb3824637
Emily Kibbler <emilykibbler@gmail.com>
2023-05-04 14:16:04 -0400

    Added scripts for plots

--- housekeeping_genes.R
+++ housekeeping_genes.R
@@ -1,9 +1,10 @@
+#R code for analysis of: https://doi.org/10.1101/2023.02.09.527892
+#Data is normalized reads of RNA seq on 23 patients with long COVID (LC) and 13 non-LC controls
 #Author: Emily Kibbler
 #This code is to evaluate potential housekeeping genes for follow-up studies by qPCR
 
 #install.packages("readxl")
 #install.packages("tidyverse")
-
 library(readxl)
 library(tidyverse)
 
@@ -40,5 +41,64 @@ documented_HKG$stdev<-NA
 for(i in 1:nrow(documented_HKG)){
   documented_HKG$stdev[i]<-sd(documented_HKG[i,5:40])
 }
-documented_HKG$var<-documented_HKG$stdev/documented_HKG$AveExpr
+documented_HKG$CV<-documented_HKG$stdev/documented_HKG$AveExpr
 view(documented_HKG)
+
+
+#candidates for best housekeeping genes
+#pull from other code, analysis done by G. Ifijeh
+
+
+#significantly expressed genes: plots
+sig_genes<-subset(reformat,reformat$Gene.name=="OR7D2"|reformat$Gene.name=="ALAS2")
+sig_genes$type<-"LC-associated"
+doc<-subset(reformat,reformat$Gene.name=="GAPDH"|reformat$Gene.name=="ACTB")
+doc$type<-"Documented HKG"
+disc<-subset(reformat,reformat$Gene.name=="CBFA2T2"|reformat$Gene.name=="SP1")
+disc$type<-"Candidate HKG"
+df<-rbind(sig_genes,doc)
+df<-rbind(df,disc)
+
+a<-subset(df,df$lc_status=="Non-LC") %>% ggplot(aes(x=factor(Gene.name,level=c("GAPDH","ACTB","CBFA2T2","SP1","OR7D2","ALAS2")),y=log2(Signal),fill=type))+
+  geom_boxplot()+
+  ggtitle("Gene expression: Non-LC")+
+  xlab("Gene name")+
+  ylab("Normalized reads: log2 scale")+
+  labs(fill="Gene type")+
+  theme(axis.text.x=element_text(angle=45,hjust=1))
+a
+
+b<-subset(df,df$lc_status=="LC") %>% ggplot(aes(x=factor(Gene.name,level=c("GAPDH","ACTB","CBFA2T2","SP1","OR7D2","ALAS2")),y=log2(Signal),fill=type))+
+  geom_boxplot()+
+  ggtitle("Gene expression: LC")  +
+  xlab("Gene name")+
+  ylab("Normalized reads: log2 scale")+
+  theme(axis.text.x=element_text(angle=45,hjust=1))+
+  labs(fill="Gene type")
+
+#install.packages("ggpubr")
+library(ggpubr)
+
+ggarrange(a,b,common.legend=TRUE)
+#ggsave("genes.png")
+
+c<-subset(df,df$lc_status=="Non-LC") %>% ggplot(aes(x=factor(Gene.name,level=c("GAPDH","ACTB","CBFA2T2","SP1","OR7D2","ALAS2")),y=log2(Signal),fill=type))+
+  geom_violin()+
+  ggtitle("Gene expression: Non-LC")+
+  xlab("Gene name")+
+  ylab("Normalized reads: log2 scale")+
+  labs(fill="Gene type")+
+  theme(axis.text.x=element_text(angle=45,hjust=1))
+
+
+d<-subset(df,df$lc_status=="LC") %>% ggplot(aes(x=factor(Gene.name,level=c("GAPDH","ACTB","CBFA2T2","SP1","OR7D2","ALAS2")),y=log2(Signal),fill=type))+
+  geom_violin()+
+  ggtitle("Gene expression: LC")  +
+  xlab("Gene name")+
+  ylab("Normalized reads: log2 scale")+
+  theme(axis.text.x=element_text(angle=45,hjust=1))+
+  labs(fill="Gene type")
+
+ggarrange(c,d,common.legend=TRUE)
+#ggsave("violin.png")
+
